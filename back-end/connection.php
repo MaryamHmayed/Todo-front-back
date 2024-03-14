e357a55562a792e7a359c914dd278d4d278d27b8
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-03-14 23:23:08 +0200

    connecting database

---
+++ back-end/connection.php
@@ -0,0 +1,15 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Max-Age: 3600');
+
+$host="localhost";
+$db_user="root";
+$pass=null;
+$db_name="todo_db";
+
+$mysqli = new mysqli($host, $db_user, $pass, $db_name);
+if (!$mysqli) {
+    die('a connection was unsuccesful');
+}
